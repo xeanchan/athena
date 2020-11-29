08e8f5b1a0718e433867d8553d07fe4d46a8b66d
wen002 <wen002@gmail.com>
2020-11-29 21:06:43 +0800

    test test

--- src/app/wireless-list/wireless-list.component.ts
+++ src/app/wireless-list/wireless-list.component.ts
@@ -9,6 +9,7 @@ export class WirelessListComponent implements OnInit {
 
   constructor() { }
 
+
   ngOnInit(): void {
   }
 
--- src/app/wireless-list/wireless-list.module.ts
+++ src/app/wireless-list/wireless-list.module.ts
@@ -4,12 +4,21 @@ import { CommonModule } from '@angular/common';
 import { WirelessListRoutingModule } from './wireless-list-routing.module';
 import { WirelessListComponent } from './wireless-list.component';
 
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+
 
 @NgModule({
   declarations: [WirelessListComponent],
   imports: [
     CommonModule,
-    WirelessListRoutingModule
+    WirelessListRoutingModule,
+    MatButtonToggleModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatDialogModule
   ]
 })
 export class WirelessListModule { }
