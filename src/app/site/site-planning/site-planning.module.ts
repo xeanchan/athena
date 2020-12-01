b40c9e037cb2b098a6c4367bf86aa16b38299ce2
wen002 <wen002@gmail.com>
2020-12-01 21:04:12 +0800

    no message

--- src/app/site/site-planning/site-planning.module.ts
+++ src/app/site/site-planning/site-planning.module.ts
@@ -4,12 +4,17 @@ import { CommonModule } from '@angular/common';
 import { SitePlanningComponent } from './site-planning.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
+import { MatButtonToggleModule, MatIconModule } from '@angular/material';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatExpansionModule
   ],
   declarations: [SitePlanningComponent],
   exports: [SitePlanningComponent]
