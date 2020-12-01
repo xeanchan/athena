ce635783af99d194da42939a01fec6bf5c4edd67
Xean <xean_chan@tynesys.com>
2020-12-01 20:35:47 +0800

    no message

--- src/app/site/site-planning/site-planning.module.ts
+++ src/app/site/site-planning/site-planning.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SitePlanningComponent } from './site-planning.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatIconModule
   ],
   declarations: [SitePlanningComponent],
   exports: [SitePlanningComponent]
