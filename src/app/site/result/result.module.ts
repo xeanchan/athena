91c1e2d9466a39789f429702d87eb023aff912e0
wen002 <wen002@gmail.com>
2020-12-16 17:41:15 +0800

    no message

--- src/app/site/result/result.module.ts
+++ src/app/site/result/result.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResultComponent } from './result.component';
 import { RouterModule } from '@angular/router';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatButtonToggleModule,
+    MatSelectModule
   ],
   declarations: [ResultComponent]
 })
-export class ResultModule { }
+export class ResultModule {
+}
