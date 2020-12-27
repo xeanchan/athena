58fcc5caa8e634e888211d5ab7135edad1bc7151
Xean <xean_chan@tynesys.com>
2020-12-28 00:08:51 +0800

    no message

--- src/app/wireless-list/wireless-list.component.ts
+++ src/app/wireless-list/wireless-list.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../service/auth.service';
 import { NewPlanningComponent } from '../new-planning/new-planning.component';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-wireless-list',
   templateUrl: './wireless-list.component.html',
   styleUrls: ['./wireless-list.component.scss']
 })
-export class WirelessListComponent implements OnInit {
+export class WirelessListComponent implements OnInit, OnDestroy {
 
   constructor(
     private authService: AuthService,
     private http: HttpClient,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private router: Router,
   ) { }
 
-  taskList;
+  taskList: any = [];
   timeInterval;
   dialogRef;
   matDialogConfig: MatDialogConfig;
@@ -27,10 +29,14 @@ export class WirelessListComponent implements OnInit {
     this.matDialogConfig = new MatDialogConfig();
     this.matDialogConfig.autoFocus = false;
 
-    // this.getList();
-    // window.setTimeout(() => {
-    //   this.startInterval();
-    // }, 5000);
+    this.getList();
+    window.setTimeout(() => {
+      this.startInterval();
+    }, 5000);
+  }
+
+  ngOnDestroy(): void {
+    window.clearInterval(this.timeInterval);
   }
 
   /**
@@ -41,6 +47,9 @@ export class WirelessListComponent implements OnInit {
       this.http.get(`${this.authService.API_URL}/taskList/sel/${this.authService.userToken}`).subscribe(
         res => {
           this.taskList = res;
+        }, err => {
+          console.log(err);
+          this.router.navigate(['/logon']);
         }
       );
     }
@@ -51,7 +60,7 @@ export class WirelessListComponent implements OnInit {
    */
   startInterval() {
     window.clearInterval(this.timeInterval);
-    window.setInterval(() => {
+    this.timeInterval = window.setInterval(() => {
       this.getList();
     }, 5000);
   }
@@ -62,6 +71,9 @@ export class WirelessListComponent implements OnInit {
   }
 
   openDialog() {
+    this.matDialogConfig.data = {
+      timeInterval: this.timeInterval
+    };
     this.dialogRef = this.dialog.open(NewPlanningComponent, this.matDialogConfig);
   }
 
